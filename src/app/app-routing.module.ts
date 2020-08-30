5a72132feae9d185d7133ee40019d9758876c90d
suvechhaya <suvechhaya@bajratechnologies.com>
2020-08-30 14:48:34 +0545

    Change app module and app routing module after adding all the components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from '../app/login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { PatientFormComponent } from './patient-form/patient-form.component';
+import { PatientRecordComponent } from './patient-record/patient-record.component';
+import { BondingAppointmentComponent } from './bonding-appointment/bonding-appointment.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: LoginComponent
+  },
+  {
+    path: 'sign-up',
+    component: SignUpComponent
+  },
+  {
+    path: 'patient-record',
+    component: PatientRecordComponent
+  },
+  {
+    path: 'patient-form',
+    component: PatientFormComponent
+  },
+  {
+    path: 'appointment-form',
+    component: BondingAppointmentComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,30 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { PatientFormComponent } from './patient-form/patient-form.component';
+import { PatientRecordComponent } from './patient-record/patient-record.component';
+import { BondingAppointmentComponent } from './bonding-appointment/bonding-appointment.component';
+import { HttpClientModule } from '@angular/common/http';
+// import { HTTP_INTERCEPTORS } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    SignUpComponent,
+    PatientFormComponent,
+    PatientRecordComponent,
+    BondingAppointmentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
+  entryComponents: [PatientFormComponent, BondingAppointmentComponent],
+  exports: [PatientFormComponent, BondingAppointmentComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
