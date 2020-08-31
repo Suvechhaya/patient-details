0ff297bdefbd1a878c95d7e4b056a348b8c0e29a
suvechhaya <suvechhaya@bajratechnologies.com>
2020-08-31 12:01:47 +0545

    Api integration and code refactoring for login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  userData = {
+    name: '',
+    password: ''
+  };
+  showModal = false;
+
+  constructor(
+    private authService: AuthService, private router: Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  public loginUser() {
+    this.authService.login(this.userData).subscribe(e => {
+        this.router.navigate(['/patient-record']);
+      });
+  }
+
+  showSignUpModal() {
+    this.showModal = true;
   }
 
 }
